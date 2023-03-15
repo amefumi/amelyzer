ea91d829f4f73c5e020f7c533130173bee0cab1d
amefumi <zty326@hotmail.com>
2023-03-15 12:07:15 +0800

    update 4 03/15/2023

--- src/network/sniffer.go
+++ src/network/sniffer.go
@@ -196,7 +196,10 @@ func parsePacket(packet gopacket.Packet) (item PacketItem, detail PacketDetail) 
 				detail.Layer3.Src = fmt.Sprintf("%s", arpPacket.SourceProtAddress)
 				detail.Layer3.Dst = fmt.Sprintf("%s", arpPacket.DstProtAddress)
 				detail.Layer3.Version = fmt.Sprintf("%d", arpPacket.Protocol)
-				detail.Layer3.Info = fmt.Sprintf("Address Resolution Protocol")
+				detail.Layer3.Info = fmt.Sprintf("Address Resolution Protocol\n\rProtocol type: %s\n\rOpcode:%d\n\r"+
+					"Sender MAC address: %s\n\rSender IP address: %s\n\rTarget MAC address: %s\n\rTarget IP address: %s",
+					arpPacket.Protocol, arpPacket.Operation, arpPacket.SourceHwAddress, arpPacket.SourceProtAddress,
+					arpPacket.DstHwAddress, arpPacket.DstProtAddress)
 				item.Source = detail.Layer3.Src
 				item.Target = detail.Layer3.Dst
 				item.Protocol = detail.Layer3.Protocol
@@ -207,46 +210,42 @@ func parsePacket(packet gopacket.Packet) (item PacketItem, detail PacketDetail) 
 			}
 			if v4Layer != nil || v6Layer != nil {
 				// Layer 4 Based on IP
-
+				tcpLayer := packet.Layer(layers.LayerTypeTCP)
+				udpLayer := packet.Layer(layers.LayerTypeUDP)
+				if tcpLayer != nil {
+					tcpPacket := tcpLayer.(*layers.TCP)
+					detail.Layer4.Protocol = "TCP"
+					var tcpFlagString = ""
+					if tcpPacket.ACK {
+						tcpFlagString += "ACK,"
+					}
+					if tcpPacket.SYN {
+						tcpFlagString += "SYN,"
+					}
+					if tcpPacket.FIN {
+						tcpFlagString += "FIN,"
+					}
+					if tcpPacket.RST {
+						tcpFlagString += "RST,"
+					}
+					detail.Layer4.Info = fmt.Sprintf("")
+					item.Protocol = "TCP"
+					item.InfoShort = fmt.Sprintf("%d -> %d Seq=%d [%s] Ack=%d Win=%d", tcpPacket.SrcPort,
+						tcpPacket.DstPort, tcpFlagString, tcpPacket.Seq, tcpPacket.Ack, tcpPacket.Window)
+				} else if udpLayer != nil {
+					udpPacket := udpLayer.(*layers.UDP)
+					detail.Layer4.Protocol = "UDP"
+					detail.Layer4.Info = fmt.Sprintf()
+					item.Protocol = "UDP"
+					item.InfoShort = fmt.Sprintf("%d -> %d, Len=%d", udpPacket.SrcPort, udpPacket.DstPort, udpPacket.Length)
+				} else {
+					// Unknown L4 Protocol
+				}
 			}
 
 		}
 	}
 
-	ipLayer := packet.Layer(layers.LayerTypeIPv4)
-	if ipLayer != nil {
-		fmt.Println("IPv4 layer detected.")
-		ip, _ := ipLayer.(*layers.IPv4)
-		// IP layer variables:
-		// Version (Either 4 or 6)
-		// IHL (IP Header Length in 32-bit words)
-		// TOS, Length, Id, Flags, FragOffset, TTL, Protocol (TCP?),
-		// Checksum, SrcIP, DstIP
-		fmt.Printf("From %s to %s\n", ip.SrcIP, ip.DstIP)
-		fmt.Println("Protocol: ", ip.Protocol)
-		fmt.Println()
-	}
-	// Let's see if the packet is TCP
-	// 判断数据包是否为TCP数据包，可解析源端口、目的端口、seq序列号、tcp标志位等
-	tcpLayer := packet.Layer(layers.LayerTypeTCP)
-	if tcpLayer != nil {
-		fmt.Println("TCP layer detected.")
-		tcp, _ := tcpLayer.(*layers.TCP)
-		// TCP layer variables:
-		// SrcPort, DstPort, Seq, Ack, DataOffset, Window, Checksum, Urgent
-		// Bool flags: FIN, SYN, RST, PSH, ACK, URG, ECE, CWR, NS
-		fmt.Printf("From port %d to %d\n", tcp.SrcPort, tcp.DstPort)
-		fmt.Println("Sequence number: ", tcp.Seq)
-		fmt.Println()
-	}
-	// Iterate over all layers, printing out each layer type
-	fmt.Println("All packet layers:")
-	for _, layer := range packet.Layers() {
-		fmt.Println("- ", layer.LayerType())
-	}
-	///.......................................................
-	// Check for errors
-	// 判断layer是否存在错误
 	if err := packet.ErrorLayer(); err != nil {
 		fmt.Println("Error decoding some part of the packet:", err)
 	}
