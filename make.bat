cc1a6089edb49598192d5f5aad67e3ca3662d620
amefumi <zty326@hotmail.com>
2023-03-19 21:47:50 +0800

    update 12 03/19/2023: Change a little on build flow.

--- make.bat
+++ make.bat
@@ -1,6 +1,4 @@
 del build\Amelyzer.exe
-assets\rsrc.exe -manifest assets\main.manifest -ico assets\app.ico -o rsrc.syso
-move rsrc.syso src\rsrc.syso
 go mod download
 cd src
 go build -ldflags="-H windowsgui"
